27157b808e142a1e3515c39c0343bc66cf1d8525
Ciarrah <50405084+Ciarrah@users.noreply.github.com>
2024-03-26 10:48:18 +0000

    Update 2b_create_any_hyt_preg.R
    Typo in rowSums ifelse statement

--- 2_SNP_outcome_derivation/2b_create_any_hyt_preg.R
+++ 2_SNP_outcome_derivation/2b_create_any_hyt_preg.R
@@ -4,7 +4,7 @@ pheno_geno=fread("pheno_geno_hyp.txt")
 
 outcome_list=grep("hyptsn_prg_MBRN|eclmps_MBRN|preeclmps_MBRN|early_preeclmps_MBRN|HELLP_MBRN",colnames(pheno_geno), value=T)
 pheno_geno$any_hyp_prg=NA
-pheno_geno$any_hyp_prg=ifelse(rowSums(as.data.frame(pheno_geno)[,paste0(outcome_list)]!=0)>0,1,0)
+pheno_geno$any_hyp_prg=ifelse(rowSums(as.data.frame(pheno_geno)[,paste0(outcome_list)]>0),1,0)
 
 table(pheno_geno$any_hyp_prg)
 pheno_geno$any_hyp_prg=as.factor(pheno_geno$any_hyp_prg)
